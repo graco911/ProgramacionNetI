a16720cc269ba3c61b393008bfa23e9cf5c61ebc
Carlos Alberto Graniel Córdova <caco572@gmail.com>
2018-02-07 09:54:00 -0600

    agregados comentarios

--- Actividad2U2/Actividad2U2/Program.cs
+++ Actividad2U2/Actividad2U2/Program.cs
@@ -15,7 +15,10 @@ namespace Actividad2U2
 
         static void Main(string[] args)
         {
+            //Mensaje de bienvenida
             Console.WriteLine("Evidencia de Aprendizaje");
+
+            //Declaracion de variables
             int categoria = 0;
             int dias = 0;
             int extras = 0;
@@ -37,6 +40,7 @@ namespace Actividad2U2
                     //leer el dato desde teclado
                     categoria = Convert.ToInt32(Console.ReadLine());
 
+                    //Si el dato es mayor manda mensaje o sale del programa
                     if (categoria < 0 || categoria > 5)
                     {
                         Console.WriteLine("Ingrese la categoria correcta.");
@@ -47,11 +51,13 @@ namespace Actividad2U2
                         Console.ReadLine();
                     }
 
+                    //Ciclo para leer los dias laborados
                     do
                     {
                         Console.WriteLine("Ingrese los dias laborados.");
                         dias = Convert.ToInt32(Console.ReadLine());
 
+                        //Si los dias son mayores a 6 envia mensaje
                         if (dias < 0 || dias > 6)
                         {
                             Console.WriteLine("Maximo 6 dias.");
@@ -61,9 +67,11 @@ namespace Actividad2U2
 
                     do
                     {
+                        //Leer las horas
                         Console.WriteLine("Ingrese las horas extras.");
                         extras = Convert.ToInt32(Console.ReadLine());
 
+                        //Si son mayor a 10 se fijan en 10
                         if (extras > 10)
                         {
                             Console.WriteLine("Solo seran pagadas 10 horas.");
@@ -72,6 +80,7 @@ namespace Actividad2U2
 
                     } while (extras > 10);
 
+                    //Funcion para calcular el sueldo
                     CalcularSueldo(categoria, dias, extras);
 
                 } while (categoria != 5);
@@ -84,7 +93,9 @@ namespace Actividad2U2
                 Console.ReadLine();
             }
         }
-
+        
+        //Funcion que calcula el sueldo recibiendo como parametros la categoria, los dias y
+        //el numerod de horas extras
         private static void CalcularSueldo(int categoria, int dias, int extras)
         {
             var sueldo = 0;
@@ -110,8 +121,9 @@ namespace Actividad2U2
                     break;
             }
 
-            Console.WriteLine("Sueldo total: " + sueldo);
-            Console.WriteLine("Horas extras : " + extra);
+            //Imprimimos en pantalla el sueldo y las horas extras
+            Console.WriteLine("Sueldo total: $" + sueldo);
+            Console.WriteLine("Horas extras : $" + extra);
             Console.WriteLine();
         }
     }
