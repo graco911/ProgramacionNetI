aadee5e7dd203f55b6719b3552bc04864032aabb
Carlos Alberto Graniel Córdova <caco572@gmail.com>
2018-02-08 00:28:28 -0600

    commit

---
+++ EvidenciaU2/EvidenciaU2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidenciaU2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (NumerosPrimos(Leer()))
+            {
+                Console.WriteLine("Es un numero primo");
+            }
+            else
+            {
+                Console.WriteLine("No es numero primo");
+            }
+
+            Console.ReadLine();
+
+        }
+
+        private static bool NumerosPrimos(int v)
+        {
+            int contador = 2;
+            var primo = true;
+            while (primo && (contador != v))
+            {
+                if (v % contador == 0)
+                {
+                    primo = false;
+                    contador++;
+                }
+            }
+            return primo;
+        }
+
+        private static int Leer()
+        {
+            var num = 0;
+            try
+            {
+                Console.WriteLine("Ingrese un numero");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                //Entrada a la excepcion
+                Console.WriteLine("Error: " + ex.Message);
+                Console.ReadLine();
+            }
+            return num;
+        }
+    }
+}
